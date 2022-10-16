7f256128fe449b60e47a015fc3daa2eeb9ad6bf3
Legadax..xo <77586407+ColdNightDEV@users.noreply.github.com>
2022-10-16 04:45:02 +0100

    Changes

--- scripts/Gsap-anims.js
+++ scripts/Gsap-anims.js
@@ -1,8 +1,8 @@
 window.onscroll = function () { scrollFunction() };
-  function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("Hero").style.transform = "scale(1.2)";
-    } else {
-        document.getElementById("Hero").style.transform = "scale(1)";
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("Hero").style.transform = "scale(1.2)";
+  } else {
+      document.getElementById("Hero").style.transform = "scale(1)";
   }
+}
---
+++ scripts/toggle.js
@@ -0,0 +1,4 @@
+function toogle() {
+    document.getElementById('bodd').classList.toggle('off');
+    document.getElementById('fram').classList.toggle('off');
+}
